5710e2c8c08304479b53cd2320922856b31ee970
mdasaduzzamanminhaz3 <mdasaduzzamanminhaz3@gmail.com>
2025-09-09 22:38:22 +0600

    try to deploy problem solve

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -221,7 +221,7 @@ SWAGGER_SETTINGS = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST',default='localhost')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',default=True,cast=bool)
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
