2f1f12ace47dc0a0979a30c9710841d11dc4c7d3
mdasaduzzamanminhaz3 <mdasaduzzamanminhaz3@gmail.com>
2025-09-09 19:16:05 +0600

    Corsheader added

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'django_filters',
+    "corsheaders",
     'rest_framework',
-   
     'djoser',
     'api',
     'product',
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
@@ -83,6 +84,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'phi_mart.wsgi.app'
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
@@ -190,6 +194,11 @@ SIMPLE_JWT = {
 
 
 DJOSER = {
+
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
+
     'SERIALIZERS': {
         'user_create':'users.serializers.UserCreateSerializer',
         'current_user':'users.serializers.UserSerializer'
@@ -207,3 +216,12 @@ SWAGGER_SETTINGS = {
       }
    }
 }
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
