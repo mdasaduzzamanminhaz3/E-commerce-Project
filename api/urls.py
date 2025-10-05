64fc8456bda85b8e66af0f5acf4e95cdfab2ae2e
mdasaduzzamanminhaz3 <mdasaduzzamanminhaz3@gmail.com>
2025-10-05 22:51:44 +0600

    SSLCommerce api added

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from rest_framework import routers
 from rest_framework_nested import routers
 from product.views import ProductViewSet,CategoryViewSet,ReviewViewSet,ProductImageViewSet
-from order.views import CartViewSet,CartItemViewSet,OrderViewSet,initiate_payment
+from order.views import CartViewSet,CartItemViewSet,OrderViewSet,initiate_payment,payment_success,payment_cancel,payment_fail
 
 router = routers.DefaultRouter()
 router.register('products',ProductViewSet,basename='products')
@@ -24,7 +24,10 @@ urlpatterns =[
     path('',include(cart_router.urls)),
     path('auth/',include('djoser.urls')),
     path('auth/',include('djoser.urls.jwt')),
-    path('payment/initiate/',initiate_payment, name='initiate-payment')
+    path('payment/initiate/',initiate_payment, name='initiate-payment'),
+    path('payment/success/',payment_success, name='payment-success'),
+    path('payment/cancel/',payment_cancel, name='payment-cancel'),
+    path('payment/fail/',payment_fail, name='payment-fail')
 ]
 
 # urlpatterns = [
